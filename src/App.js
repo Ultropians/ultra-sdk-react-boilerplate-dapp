2d9d4bc7f7ba020e66cbb3d0abce5ee0d0a9cb10
Jesse van Eekelen <jesse-vaneekelen@ziggo.nl>
2023-05-16 15:08:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useUltra } from '@ultra-alliance/react-ultra';
+import { Button, Typography } from '@mui/material';
 
-function App() {
+const App = () => {
+  const {account, isAuthenticated, login, hasAuthError } = useUltra();
+ 
+  if (!isAuthenticated) {
+    return (
+      <div className="centeredContent">
+        <div className="connectButton"> 
+          <Button color="primary" variant="contained" onClick={login}>
+            Login
+          </Button>
+          {hasAuthError && (
+            <div className="authErrorMessage"> 
+              <Typography variant="body1" color="error">
+                Login failed, please connect your wallet.
+              </Typography>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centeredContent">
+      <Typography variant="body1">UOS Balance: {account.data.core_liquid_balance}</Typography>
     </div>
   );
-}
+};
 
 export default App;
